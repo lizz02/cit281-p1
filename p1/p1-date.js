a02626a70e109894317f86bf345a925d1bd95baa
lizz02 <82979075+lizz02@users.noreply.github.com>
2021-05-30 14:59:49 -0700

    Delete p1 directory

--- p1/p1-date.js
+++
@@ -1,7 +0,0 @@
-/*
-    CIT 281 Project 1
-    Name: Elizabeth Yost
-*/
-let daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-let today = new Date()
-console.log(daysOfWeek[(today.getDay())- 1])
